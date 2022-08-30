d41e1bca1904776272c2694806dc68fd11717ead
RoryEiffe <roryeiffe8@gmail.com>
2022-08-30 13:50:43 -0400

    Pushing JUnit exercises

---
+++ week5/Calculator/README.md
@@ -0,0 +1,18 @@
+For this exercise, you will write JUnit test cases to achieve 100% method and line coverage in the calculator class.
+
+You will utilize git branching to push your code.
+In git bash, create and checkout your branch:
+
+```
+git branch -b <your-name>
+```
+
+Work on the project, make your changes. 
+
+To push it up:
+```
+git add .
+git commit -m "Finished exercised."
+git push origin <branch-name>
+```
+
---
+++ week5/Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return a/b;
+    }
+
+    public boolean isEven(int a) {
+        if(a % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isOdd(int a) {
+        if(a % 2 == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
