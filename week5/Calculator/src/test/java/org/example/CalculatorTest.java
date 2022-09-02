bb9118a0cd005aa01063313570b2864d34e2b82d
alopez651 <ana.lopez6517@gmail.com>
2022-09-02 13:40:00 -0700

    calculator exercise

--- week5/Calculator/src/test/java/org/example/CalculatorTest.java
+++ week5/Calculator/src/test/java/org/example/CalculatorTest.java
@@ -9,4 +9,37 @@ public class CalculatorTest {
     public static void test() {
         assertEquals(1, 1);
     }
+    @Test
+    public static void test1(int a, int b){
+        Calculator calculator = new Calculator();
+        assertEquals(7, calculator.add(3,4));
+    }
+    @Test
+    public static  void test2(int a, int b){
+        Calculator calculator = new Calculator();
+        assertEquals(10, calculator.subtract(20,10));
+    }
+    @Test
+    public static void test3(int a, int b){
+        Calculator calculator = new Calculator();
+        assertEquals(25, calculator.multiply(5, 5));
+        assertEquals(25, calculator.divide(625, 25));
+    }
+    @Test
+    public static void test4(int a, int b){
+        Calculator calculator = new Calculator();
+        assertEquals(25, calculator.divide(625, 25));
+    }
+    @Test
+    public static void test5(int a){
+        Calculator calculator = new Calculator();
+        assertEquals(25, calculator.isEven(5));
+    }
+    @Test
+    public static void test6(int a){
+        Calculator calculator= new Calculator();
+        assertEquals(100, calculator.isOdd(10));
+    }
+
+
 }
