5b62fb15d36e9ea1767af837c13dcb085dc1996b
LwFrahm <luke.2017@hotmail.com>
2022-09-06 16:02:53 -0500

    Finished exercise

--- week5/Calculator/src/test/java/org/example/AppTest.java
+++ week5/Calculator/src/test/java/org/example/AppTest.java
@@ -1,21 +1,25 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
     /**
-     * Rigorous Test :-)
+     * Create the test case
+     *
+     * @param testName name of the test case
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+
+    /**
+     * Rigourous Test :-)
+     */
+
 }
--- week5/Calculator/src/test/java/org/example/CalculatorTest.java
+++ week5/Calculator/src/test/java/org/example/CalculatorTest.java
@@ -1,12 +1,47 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    @Test
-    public static void test() {
-        assertEquals(1, 1);
+
+    Calculator calculator;
+
+    @BeforeEach
+    public void init() {
+        this.calculator = new Calculator();
     }
+
+    @Test
+    public void testAdd() {
+        assertEquals(23, calculator.add(10, 13));
+    }
+
+    @Test
+    public void testSubtact() {
+        assertEquals(-3, calculator.subtract(10, 13));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(130, calculator.multiply(10, 13));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(26, 13));
+    }
+
+    @Test
+    public void testIsEven() {
+        assertEquals(false, calculator.isEven( 13));
+    }
+
+    @Test
+    public void testIsOdd() {
+        assertEquals(true, calculator.isOdd( 13));
+    }
+
+
 }
